327ae656ebc5ef20c3a7661e1c4e32fd306b6873
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-05-31 19:39:43 -0600

    Primer Commit

---
+++ Listas/Vistas/ViewReporteContactos.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Listas.Vistas
+{
+    public partial class ViewReporteContactos : ContentPage
+    {
+        public ViewReporteContactos()
+        {
+            InitializeComponent();
+        }
+    }
+}
