0b8ef3f8a6a0cff812a76208df46e7238ea26824
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-05-31 20:57:02 -0600

    Creacion de Formulario de Docente

--- Listas/ViewModel/VMAlumno.cs
+++ Listas/ViewModel/VMAlumno.cs
@@ -1,10 +1,81 @@
 ﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
 namespace Listas.ViewModel
 {
-    public class VMAlumno
+    public class VMAlumno : INotifyPropertyChanged
     {
         public VMAlumno()
         {
+            FaltarClase = new Command(
+                     () => {
+                        Faltas = Faltas + 1;
+
+                        if (Faltas <= 8) {
+                            Reporte = "El alumno "+ Nombre +" tiene derecho a examen.";
+                        }
+                        else
+                        {
+                            Reporte = "El alumno "+ Nombre +"  perdio drecho a examen.";
+
+                        }
+
+                    }
+                );
         }
+
+        String nombre;
+
+        public String Nombre { get => nombre;
+            set {
+                nombre = value;
+                var args = new PropertyChangedEventArgs( nameof(Nombre) );
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        String identidad;
+
+        public String Identidad {
+            get => identidad;
+            set {
+
+                identidad = value;
+                var args = new PropertyChangedEventArgs(nameof(Identidad));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
+
+        int faltas = 0;
+        public int Faltas {
+
+            get => faltas;
+            set {
+
+                faltas = value;
+                var args = new PropertyChangedEventArgs(nameof(Faltas));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+
+        }
+
+        string reporte="";
+        public string Reporte {
+            get => reporte;
+            set  {
+                reporte = value;
+                var args = new PropertyChangedEventArgs(nameof(Reporte));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
+
+        public Command FaltarClase { get;  }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
---
+++ Listas/ViewModel/VMDocente.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Listas.ViewModel
+{
+    public class VMDocente : INotifyPropertyChanged
+    {
+        public VMDocente()
+        {
+
+            DarClase = new Command(
+                    () => {
+                        HorasClaseAcumulada = HorasClaseAcumulada + HorasClase;
+                        Reporte = "El docente " + Nombre + " Tiene por Salario: " + HorasClaseAcumulada * 300;
+                    }
+                );
+
+            ResetHorasClase = new Command
+                (
+
+                    () => {
+
+                        HorasClaseAcumulada = 0;
+                        Reporte = "El docente " + Nombre + " Tiene por Salario: " + HorasClaseAcumulada * 300;
+                    }
+
+                );
+
+        }
+
+        String nombre;
+
+        public String Nombre
+        {
+            get => nombre;
+            set
+            {
+                nombre = value;
+                var args = new PropertyChangedEventArgs(nameof(Nombre));
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        String identidad;
+
+        public String Identidad
+        {
+            get => identidad;
+            set
+            {
+
+                identidad = value;
+                var args = new PropertyChangedEventArgs(nameof(Identidad));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
+
+        int horasClase = 0;
+        public int HorasClase
+        {
+
+            get => horasClase;
+            set
+            {
+                horasClase = value;
+                var args = new PropertyChangedEventArgs(nameof(HorasClase));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
+
+        int horasClaseAcumulada = 0;
+        public int HorasClaseAcumulada {
+            get => horasClaseAcumulada;
+            set {
+
+                horasClaseAcumulada = value;
+                var args = new PropertyChangedEventArgs(nameof(HorasClaseAcumulada));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
+
+        string reporte = "";
+        public string Reporte
+        {
+            get => reporte;
+            set
+            {
+                reporte = value;
+                var args = new PropertyChangedEventArgs(nameof(Reporte));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
+
+        public Command DarClase { get; }
+        public Command ResetHorasClase { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
