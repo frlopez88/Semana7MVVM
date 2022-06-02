f121b440e0de2fc8da1bdaeb589f2216fea19f30
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-06-01 20:21:52 -0600

    Commit de Alumnos y Listas

--- Listas/ViewModel/VMAlumno.cs
+++ Listas/ViewModel/VMAlumno.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
 using Xamarin.Forms;
+using Listas.Model;
+using System.Collections.ObjectModel;
 
 namespace Listas.ViewModel
 {
@@ -10,27 +12,69 @@ namespace Listas.ViewModel
         {
             FaltarClase = new Command(
                      () => {
-                        Faltas = Faltas + 1;
+                         Faltas = Faltas + 1;
 
-                        if (Faltas <= 8) {
-                            Reporte = "El alumno "+ Nombre +" tiene derecho a examen.";
-                        }
-                        else
-                        {
-                            Reporte = "El alumno "+ Nombre +"  perdio drecho a examen.";
+                         if (Faltas <= 8) {
+                             Reporte = "El alumno " + Nombre + " tiene derecho a examen.";
+                         }
+                         else
+                         {
+                             Reporte = "El alumno " + Nombre + "  perdio drecho a examen.";
+
+                         }
+
+                     }
+                );
+
+            Guardar = new Command(
+                    () => {
+
+
+                        ModelAlumno x = new ModelAlumno() {
+                            nombre = Nombre,
+                            identidad = Identidad,
+                            cantidadFaltas=Faltas
+                        };
+
+
+                        listaGuardar.Add(x);
+
+                    }
+
+                );
+
+            Buscar = new Command(
+                    () => {
+
+                        ModelAlumno temporal = new ModelAlumno();
+
+                        for (int i = 0; i < listaGuardar.Count; i++) {
+
+                            temporal = listaGuardar[i];
+
+                            if (temporal.identidad == Identidad) {
+
+                                Nombre = temporal.nombre;
+                                Faltas = temporal.cantidadFaltas;
+                                Reporte = temporal.calculoFaltas();
+
+                            }
 
                         }
 
                     }
                 );
+
         }
+
+        public ObservableCollection<ModelAlumno> listaGuardar { get; set; } = new ObservableCollection<ModelAlumno>();
 
         String nombre;
 
         public String Nombre { get => nombre;
             set {
                 nombre = value;
-                var args = new PropertyChangedEventArgs( nameof(Nombre) );
+                var args = new PropertyChangedEventArgs(nameof(Nombre));
                 PropertyChanged?.Invoke(this, args);
             }
         }
@@ -62,10 +106,10 @@ namespace Listas.ViewModel
 
         }
 
-        string reporte="";
+        string reporte = "";
         public string Reporte {
             get => reporte;
-            set  {
+            set {
                 reporte = value;
                 var args = new PropertyChangedEventArgs(nameof(Reporte));
                 PropertyChanged?.Invoke(this, args);
@@ -73,9 +117,19 @@ namespace Listas.ViewModel
             }
         }
 
+        public Command Guardar { get; }
+
+
+        public Command Buscar { get; }
+
         public Command FaltarClase { get;  }
 
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }
+
+// MVVM:
+// Model(Clases: Primer Parcial)
+// View(Front END)
+// ViewModel(Son Clases para Manejar el Front END)
